3831f9851cc2af22dddb286fe34c32601ccdf0d9
Anton <antonafonin56@gmail.com>
2022-09-06 20:05:35 +0100

    start

---
+++ app.js
@@ -0,0 +1,25 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privet: false
+};
+
+let moviess = '';
+let rating = +'';
+
+for (let i = 0; i < numberOfFilms; i++) {
+    // personalMoviesDB.movies[]
+    moviess = prompt('Какой фильм вы посмотрели?', '');
+    rating = +prompt('На сколько оценете его');
+    personalMoviesDB.movies[moviess] = rating;
+}
+
+const actor = prompt('Как зовут вашего любимого актера?');
+
+personalMoviesDB.actors['actorName'] = actor;
+
+console.log(personalMoviesDB);
