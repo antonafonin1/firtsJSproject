5e5b2896ba93ff9d7654e6441637daaa81de2bc1
Anton <antonafonin56@gmail.com>
2022-09-07 21:30:27 +0100

    actor

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let rating = +'';
 
 for (let i = 0; i < numberOfFilms; i++) {
     // personalMoviesDB.movies[]
-    moviess = prompt('Какой фильм вы посмотрели?', '');
+    moviess = prompt('Один из последних просмотренных фильмов?', '');
     rating = +prompt('На сколько оценете его');
     personalMoviesDB.movies[moviess] = rating;
 }
