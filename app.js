411a013f60af16815a9b2505815645c935d45a41
Anton <antonafonin56@gmail.com>
2022-09-08 16:27:36 +0100

    automation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strick";
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMoviesDB = {
@@ -8,15 +10,30 @@ const personalMoviesDB = {
     privet: false
 };
 
+if (10 <= personalMoviesDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMoviesDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
 let moviess = '';
 let rating = +'';
 
-for (let i = 0; i < numberOfFilms; i++) {
+for (let i = 0; i < 2; i++) {
     // personalMoviesDB.movies[]
     moviess = prompt('Один из последних просмотренных фильмов?', '');
-    rating = +prompt('На сколько оценете его');
-    personalMoviesDB.movies[moviess] = rating;
-}
+    rating = prompt('На сколько оценете его');
+
+    if (moviess != null && rating != null && moviess != '' && rating != '' && moviess.length < 50) {
+        personalMoviesDB.movies[moviess] = rating;
+    } else { 
+        i--;
+    }
+} 
+
+
 
 const actor = prompt('Как зовут вашего любимого актера?');
 
